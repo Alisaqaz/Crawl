78508a1f6851c19dcde56c999d75cecd5c11cd49
alisalee <15529496259@163.com>
2018-08-18 16:04:41 +0800

    Update Crawls.go

--- Crawl/Crawls.go
+++ Crawl/Crawls.go
@@ -6,9 +6,11 @@ import (
 	"os"
 	"io/ioutil"
 	"encoding/xml"
+	"regexp"
+	"strings"
 )
 
-var urlChanne = make(chan string, 20000)// url channe distribute task
+var urlChanne = make(chan string,200000)// url channe distribute task
 type Sconfig struct {
 	XMLName xml.Name `xml:"note"`   //point to outer label
 	SeedUrl string `xml:"url"`   // read url and save url into SeedUrl
@@ -34,9 +36,6 @@ func getSeedUrl(){
 	}
 	Seedurl=v.SeedUrl
 	fmt.Println(Seedurl)
-
-
-
 }
 func GetJokes(url string){
 	doc, err := goquery.NewDocument(url)
@@ -54,21 +53,20 @@ func GetJokes(url string){
 
 func main(){
 	getSeedUrl()
-	getUrl(Seedurl)
-	//for i:=0;i< len(urlChanne);i++{
-	//	go getUrl()
-	//}
+	getUrl(Seedurl,0)
+
+	     num :=0
+	for{
 	for v:=range urlChanne{
-		fmt.Println("sds"+v)
-		getUrl(v)
-	}
-	//for i:=0;i<= len(urlChanne);i++{
-	//	<-urlChanne
-	//}
-	defer   fmt.Println(2343)
-	println(urlChanne)
-	//	GetJokes(url)             getUrl
-	///getUrlFromLink(url)
+	    num=num+1
+	    fmt.Println("num="+string(num))
+		reg :=regexp.MustCompile("javascript")
+		if v!="" && reg.MatchString(v)==false{
+			getUrl(v,-2)
+		}
+	}}
+	fmt.Println(len(urlChanne))
+
 }
 
 func getUrlFromLink(url string){
@@ -78,13 +76,13 @@ func getUrlFromLink(url string){
 	 	fmt.Println(e)
 	 }
 	 link :=doc1.Find("link")
-	//href :=link.Eq(1).Find("link")
 	 link.Each(func(f int, selection *goquery.Selection) {
 	 	temp1,_:=selection.Attr("href")
 	 	fmt.Println(temp1)
 
 	 })
 }
+
 func getUrlFromsrc(url string){
 	   fmt.Println("src")
 	   doc2, err2:=goquery.NewDocument(url)
@@ -94,26 +92,54 @@ func getUrlFromsrc(url string){
 	   doc2.Find("href")
 	   
 }
-func getUrl(url string){
-	fmt.Println(url)
-//	fmt.Println(2343)
+func getUrl(url string,sum int){
+
+	fmt.Println("url"+url)
+
+ reg :=regexp.MustCompile("https")
+	reg2 :=regexp.MustCompile("http")
+	if reg.MatchString(url)==false && reg2.MatchString(url)==false{
+	url="https:"+url
+	}else if reg.MatchString(url)==false && reg2.MatchString(url)==true{
+	url=strings.Replace(url,"http","https",1)
+	}
 	doc3,err3 :=goquery.NewDocument(url)
 	if err3!=nil{
 	fmt.Println(err3)
-	}
-
+	}  else{
 	doc3.Find("body a").Each(func(index int,item *goquery.Selection){
 		LTag :=item
 		Link,_:=LTag.Attr("href")
-	//	linktext :=LTag.Text()
-	//	fmt.Println("dfs"+Link)
-		urlChanne <-Link
+
+		if Link!=""{
+
+		if sum!=0{
+
+			urlChanne <-Link
+			fmt.Println(Link)
+
+		}else{	urlChanne <-Link
+			fmt.Println("dfs"+Link)
+		}
+		}
 	})
 	doc3.Find("body script").Each(func(index int,item *goquery.Selection){
 		LTag :=item
 		Link,_:=LTag.Attr("src")
+		if Link!=""{
+
+		if sum!=0{
+
 		urlChanne <-Link
-//		fmt.Println("shdjsa"+Link)
+			fmt.Println(Link)
+
+		}else{
+			urlChanne <-Link
+			fmt.Println("shdjsa"+Link)
+		}
+		}
 	})
+
+	}
 
 }
